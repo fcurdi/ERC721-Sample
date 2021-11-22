af35e5c1578c9127e388527d3c8d900fb9c2ea2f
fcurdi <franciscocurdi@gmail.com>
2021-11-22 16:21:08 -0300

    initial implementation

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -3,12 +3,19 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-//TODO events
+//TODO emit events, ERC721Metadata
 contract MyNFT is IERC721 {
     mapping(address => uint256) _balances;
 
+    /// The zero address indicates an invalid NFT.
     mapping(uint256 => address) _owners;
+
+    mapping(address => mapping(address => bool)) _operators;
+
+    /// The zero address indicates there is no approved address.
+    mapping(uint256 => address) _approvedAddresses;
 
     function balanceOf(address owner) external view returns (uint256 balance) {
         require(owner != address(0), "Invalid address");
@@ -20,101 +27,74 @@ contract MyNFT is IERC721 {
         require(owner != address(0));
     }
 
-    /// @notice Transfers the ownership of an NFT from one address to another address
-    /// @dev Throws unless `msg.sender` is the current owner, an authorized
-    ///  operator, or the approved address for this NFT. Throws if `_from` is
-    ///  not the current owner. Throws if `_to` is the zero address. Throws if
-    ///  `_tokenId` is not a valid NFT. When transfer is complete, this function
-    ///  checks if `_to` is a smart contract (code size > 0). If so, it calls
-    ///  `onERC721Received` on `_to` and throws if the return value is not
-    ///  `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`.
-    /// @param from The current owner of the NFT
-    /// @param to The new owner
-    /// @param tokenId The NFT to transfer
-    /// @param data Additional data with no specified format, sent in call to `_to`
     function safeTransferFrom(
         address from,
         address to,
         uint256 tokenId,
         bytes calldata data
     ) external {
-        // TODO
+        _safeTransferFrom(from, to, tokenId, data);
     }
 
-    /// @notice Transfers the ownership of an NFT from one address to another address
-    /// @dev This works identically to the other function with an extra data parameter,
-    ///  except this function just sets data to "".
-    /// @param from The current owner of the NFT
-    /// @param to The new owner
-    /// @param tokenId The NFT to transfer
     function safeTransferFrom(
         address from,
         address to,
         uint256 tokenId
     ) external {
-        // TODO
+        _safeTransferFrom(from, to, tokenId, "");
     }
 
-    /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
-    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
-    ///  THEY MAY BE PERMANENTLY LOST
-    /// @dev Throws unless `msg.sender` is the current owner, an authorized
-    ///  operator, or the approved address for this NFT. Throws if `_from` is
-    ///  not the current owner. Throws if `_to` is the zero address. Throws if
-    ///  `_tokenId` is not a valid NFT.
-    /// @param from The current owner of the NFT
-    /// @param to The new owner
-    /// @param tokenId The NFT to transfer
     function transferFrom(
         address from,
         address to,
         uint256 tokenId
     ) external {
-        // TODO
+        // TODO validate from, to not zero addresses?
+        address owner = _owners[tokenId];
+        require(owner != address(0), "NFT is not valid");
+        require(
+            msg.sender == owner ||
+                _operators[owner][msg.sender] ||
+                _approvedAddresses[tokenId] == msg.sender,
+            "Sender is not authorized to make a transfer"
+        );
+        require(from == owner, "From address must be the NFT owner");
+        require(to != address(0));
+        _owners[tokenId] = to;
+        // TODO refactor this with _safeTransferFrom.
     }
 
-    /// @notice Change or reaffirm the approved address for an NFT
-    /// @dev The zero address indicates there is no approved address.
-    ///  Throws unless `msg.sender` is the current NFT owner, or an authorized
-    ///  operator of the current owner.
-    /// @param to The new approved NFT controller
-    /// @param tokenId The NFT to approve
     function approve(address to, uint256 tokenId) external {
-        // TODO
+        address owner = _owners[tokenId];
+        require(
+            msg.sender == owner || _operators[owner][msg.sender],
+            "Sender is not owner nor operator"
+        );
+        _approvedAddresses[tokenId] = to;
     }
 
-    /// @notice Enable or disable approval for a third party ("operator") to manage
-    ///  all of `msg.sender`'s assets
-    /// @dev Emits the ApprovalForAll event. The contract MUST allow
-    ///  multiple operators per owner.
-    /// @param operator Address to add to the set of authorized operators
-    /// @param _approved True if the operator is approved, false to revoke approval
     function setApprovalForAll(address operator, bool _approved) external {
-        // TODO
+        // TODO check that operator is not zero address?
+        _operators[msg.sender][operator] = _approved;
     }
 
-    /// @notice Get the approved address for a single NFT
-    /// @dev Throws if `_tokenId` is not a valid NFT.
-    /// @param tokenId The NFT to find the approved address for
-    /// @return operator The approved address for this NFT, or the zero address if there is none
     function getApproved(uint256 tokenId)
         external
         view
         returns (address operator)
     {
-        // TODO
+        address owner = _owners[tokenId];
+        require(owner != address(0), "NFT is not valid");
+        operator = _approvedAddresses[tokenId];
     }
 
-    /// @notice Query if an address is an authorized operator for another address
-    /// @param owner The address that owns the NFTs
-    /// @param operator The address that acts on behalf of the owner
-    /// @return True if `_operator` is an approved operator for `_owner`, false otherwise
     function isApprovedForAll(address owner, address operator)
         external
         view
         returns (bool)
     {
-        // TODO
+        // TODO check that operator is not zero address?
+        return _operators[owner][operator];
     }
 
     /// @notice Query if a contract implements an interface
@@ -130,5 +110,47 @@ contract MyNFT is IERC721 {
     {
         // TODO
         // IERC165
+    }
+
+    function _safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) private {
+        // TODO validate from, to not zero addresses?
+        address owner = _owners[tokenId];
+        require(owner != address(0), "NFT is not valid");
+        require(
+            msg.sender == owner ||
+                _operators[owner][msg.sender] ||
+                _approvedAddresses[tokenId] == msg.sender,
+            "Sender is not authorized to make a transfer"
+        );
+        require(from == owner, "From address must be the NFT owner");
+        require(to != address(0));
+        _owners[tokenId] = to;
+
+        // check if address is smart contract (code size > 0)
+        uint256 size;
+        assembly {
+            size := extcodesize(to)
+        }
+        bool isSmartContract = size > 0;
+        //
+
+        if (isSmartContract) {
+            IERC721Receiver receiver = IERC721Receiver(to);
+            bytes4 result = receiver.onERC721Received(
+                msg.sender,
+                from,
+                tokenId,
+                data
+            );
+            require(
+                result == receiver.onERC721Received.selector,
+                "To address rejected the transfer"
+            );
+        }
     }
 }
